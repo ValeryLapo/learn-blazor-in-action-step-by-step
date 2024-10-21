19d99829064ba1213f3c8c156196445135bcfe5a
ValeriyLappo <i@vlappo.ru>
2024-10-21 18:03:15 +0300

    up to 5 (app. A) - database

---
+++ BlazingTrails.Client/BlazingTrails.Api/Persistence/BlazingTrailsContext.cs
@@ -0,0 +1,18 @@
+﻿using BlazingTrails.Api.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingTrails.Api.Persistence;
+
+public class BlazingTrailsContext(DbContextOptions<BlazingTrailsContext> options) : DbContext(options)
+{
+    public DbSet<Trail> Trails => Set<Trail>();
+    public DbSet<RouteInstruction> RouteInstructions => Set<RouteInstruction>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new TrailConfig());
+        modelBuilder.ApplyConfiguration(new RouteInstructionConfig());
+    }
+}
