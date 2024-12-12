1f9e489f81ac7bbf80f65c2e5419e671c000845d
ValeriyLappo <i@vlappo.ru>
2024-12-12 18:18:51 +0300

    feat: done appendix B

---
+++ BlazingTrails.Client/BlazingTrails.Api/Features/Home/GetTrailsEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.ApiEndpoints;
+using BlazingTrails.Api.Persistence;
+using BlazingTrails.Shared.Features.Home.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingTrails.Api.Features.Home;
+
+public class GetTrailsEndpoint: BaseAsyncEndpoint.WithRequest<int>.WithResponse<GetTrailsRequest.Response>
+{
+    private readonly BlazingTrailsContext _database;
+    public GetTrailsEndpoint(BlazingTrailsContext database)
+    {
+        _database = database;
+    }
+    [HttpGet(GetTrailsRequest.RouteTemplate)]
+    public override async Task<ActionResult<GetTrailsRequest.Response>> HandleAsync(int trailId, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var trails = await _database.Trails
+            .ToListAsync(cancellationToken);
+
+        var response = new GetTrailsRequest.Response(trails.Select(t => new GetTrailsRequest.Trail(
+            t.Id,
+            t.Name,
+            t.Image,
+            t.Location,
+            t.TimeInMinutes,
+            t.Length,
+            t.Description)));
+
+        return Ok(response);
+    }
+}
---
+++ BlazingTrails.Client/BlazingTrails.Client/Features/Home/Shared/GetTrailsHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using BlazingTrails.Shared.Features.Home.Shared;
+using MediatR;
+
+namespace BlazingTrails.Client.Features.Home.Shared;
+
+public class GetTrailsHandler(HttpClient client): IRequestHandler<GetTrailsRequest, GetTrailsRequest.Response>
+{
+    public async Task<GetTrailsRequest.Response?> Handle(GetTrailsRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await client.GetFromJsonAsync<GetTrailsRequest.Response>(GetTrailsRequest.RouteTemplate, cancellationToken: cancellationToken);
+        }
+        catch (HttpRequestException ex)
+        {
+            return default!;
+        }
+    }
+}
